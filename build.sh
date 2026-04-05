bb5ca5ad4dd2df5ad727fd9066e3b01f6e561a70
F3FFO <38475841+F3FFO@users.noreply.github.com>
2026-04-05 10:55:42 +0200

    Improve logic

--- build.sh
+++ build.sh
@@ -5,17 +5,19 @@ rm -f stub-unaligned.apk stub.apk stub.apk.idsig
 
 # Value of minSdk
 MIN_SDK="${1:-21}"
+# Output APK name (default stub.apk)
+APK_NAME="${2:-stub.apk}"
 
 # Make APK
 aapt package -M AndroidManifest.xml -I $ANDROID_HOME/platforms/android-35/android.jar -F stub-unaligned.apk --min-sdk-version $MIN_SDK --target-sdk-version 35
 
-# Alingn APK
-zipalign 4 stub-unaligned.apk stub.apk
+# Align APK
+zipalign 4 stub-unaligned.apk "$APK_NAME"
 rm stub-unaligned.apk
 
 # Sign APK
-apksigner sign --cert data/cert.pem --key data/cert.pk8 --v1-signing-enabled true --v2-signing-enabled false --v3-signing-enabled false --v4-signing-enabled false stub.apk
+apksigner sign --cert data/cert.pem --key data/cert.pk8 --v1-signing-enabled true --v2-signing-enabled false --v3-signing-enabled false --v4-signing-enabled false "$APK_NAME"
 
-rm stub.apk.idsig
+rm "${APK_NAME}.idsig" 2>/dev/null || true
 
-echo "Created: stub.apk"
+echo "Created: $APK_NAME"
